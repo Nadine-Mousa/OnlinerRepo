668b8a589fa1be58bfb0782fce9567205a1c539d
Nadine <nadinemousa1.outlook.com>
2022-04-21 17:37:59 +0200

    solve conflict

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -156,16 +156,3 @@ body {
 
 
 
-<<<<<<< HEAD
-=======
-
-new line at 159 from master
-{{-- this is added form master  --}}
-new branch edit in line 159
-
-this line from nadine
-
-this line from howaida
-
-
->>>>>>> refs/remotes/origin/master
